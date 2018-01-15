65c14104151810744489b601d84be827f48fda05
Ruben <rubensancor@opendeusto.es>
2018-01-15 17:57:33 +0100

    Models

--- home/models.py
+++ home/models.py
@@ -4,3 +4,26 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    email = models.CharField(max_length = 45)
+    password = models.CharField(max_length = 20)
+    name = models.CharField(max_length = 20)
+    username = models.CharField(max_length = 20)
+    phone_number = models.IntegerField()
+
+
+class Course(models.Model):
+    id = models.CharField(max_length = 10)
+    name = models.CharField(max_length = 20)
+    description = models.TextField()
+    latitude = models.IntegerField()
+    longitude = models.IntegerField()
+    profesorEmail = models.CharField(max_length = 45)
+    price = models.IntegerField()
+    likes = models.IntegerField()
+
+#class Tags(models.Model):
+
+# "tags": [
+#  "string"
--- home/views.py
+++ home/views.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.http import HttpResponse
 from django.shortcuts import render
+import urllib3, json, requests
+
+# Courses url
+courses_url = "http://www.athenea-project.org/courses-microservice/api/course/all"
 
 # Create your views here.
+
+def index(request):
+  payload = {'accept':'application/json'}
+  data = requests.get('http://www.athenea-project.org/courses-microservice/api/course/all', params=payload)  
+  
+  print(data)
+  return HttpResponse("Hello, world. You're at the polls index.")
